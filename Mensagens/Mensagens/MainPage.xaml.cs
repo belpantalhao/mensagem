4892309e4c64e9397e29f56298d739f8cacc3daa
IsabelPantalhao <100951547+belpantalhao@users.noreply.github.com>
2022-11-08 12:16:53 -0300

    Add files via upload

---
+++ Mensagens/Mensagens/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mensagens
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var result = DisplayAlert("Alerta", "Deseja realmente sair?", "ok");
+        }
+    }
+}
---
+++ Services.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Appmensagens.Model;
+
+namespace AppMensagens'.Services
+{
+    public class DataServices
+    {
+        public static async Task<Conselho> GetConselho(string conselho)
+        {
+
+
+            string queryString = "https://api.adviceslip.com/advice" + conselho;
+
+            dynamic resultado = await getDataFromService(queryString).ConfigureAwait(false);
+
+        }
+    }
+}
